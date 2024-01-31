2b756a4d58e7228bc11cb6cd3e3c8ba063df469b
prink <prinkaldoshi@gmail.com>
2024-01-31 16:42:29 +0530

    Hero Backg done.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,21 @@ export default {
         dgreen: "#2fff2f",
         lorange: "#f8d6b3",
         dorange: "#ffa07a"
-      }
+      },
+      animation: {
+        marquee: "marquee 10s linear infinite",
+        marquee2: "marquee2 10s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
   plugins: [],
