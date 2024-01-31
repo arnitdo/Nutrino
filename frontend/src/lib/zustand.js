8ade20ea2576891036aefe0dacd61bee04149c37
arnitdo <arnav.nitin.deo@gmail.com>
2024-01-31 23:45:57 +0530

    Fix store and stuff

--- frontend/src/lib/zustand.js
+++ frontend/src/lib/zustand.js
@@ -1,36 +1,37 @@
-import { create } from "zustand";
+import {create} from "zustand";
 
 const store = create((set) => ({
-  backend_url: "https://nutrino-backend.up.railway.app/api",
-  api:"ed48b872724640348407c671ce3bdd5d",
-  message: "",
-  setMessage: (item) => set(() => ({ message: item })), // set is a function that takes a function
-  toast: false,
-  setToast: (item) => set(() => ({ toast: item })),
-  user: null,
-  setUser: (item) => set(() => ({ user: item })),
-  auth: false,
-  setAuth: (item) => set(() => ({ auth: item })),
-  Logout: () => set(() => ({ user: null, auth: false })),
-  getUser: (token) => set(async () => ({user: await getMe(token)})),
+	backend_url: "https://nutrino-backend.up.railway.app/api",
+	api: "ed48b872724640348407c671ce3bdd5d",
+	message: "",
+	setMessage: (item) => set(() => ({message: item})), // set is a function that takes a function
+	type: "info",
+	setType: (item) => set(() => ({type: item})),
+	toast: false,
+	setToast: (item) => set(() => ({toast: item})),
+	user: null,
+	setUser: (item) => set(() => ({user: item})),
+	auth: false,
+	setAuth: (item) => set(() => ({auth: item})),
+	Logout: () => set(() => ({user: null, auth: false})),
+	getUser: (token) => set(async () => ({user: await getMe(token)})),
 }));
 
-const getMe = async(token) => {
-  console.log("fired 2")
-  const url = import.meta.env.VITE_BACKEND_URL
-  const res = await fetch(`${url}/auth/me`, {
-      method: "GET",
-      headers: {
-          "Content-Type": "application/json",
-          "auth-token": token
-      }
-  })
-  const data = await res.json()
-  if(!data.error){
-    return data.user
-  }else {
-    return null
-  }
+const getMe = async (token) => {
+	const url = import.meta.env.VITE_BACKEND_URL
+	const res = await fetch(`${url}/auth/me`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"auth-token": token
+		}
+	})
+	const data = await res.json()
+	if (!data.error) {
+		return data.user
+	} else {
+		return null
+	}
 }
 
 export default store;
