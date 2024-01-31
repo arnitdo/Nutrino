ab272e46cf21bb8f86a40c458a1fe8cbdab81dba
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2024-01-31 13:35:21 +0530

    added image captioning api flask

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import cv2
 import easyocr
 import numpy as np
+from transformers import pipeline
 
 app = Flask(__name__)
 reader = easyocr.Reader(['en'], gpu=False)
@@ -39,6 +40,21 @@ def ocr():
             'error': str(e)
         }
     return jsonify(response)
+
+
+image_to_text_pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+
+@app.route('/imagecaptioning', methods=['POST'])
+def imagecaptioning():
+    try:
+        import os
+        image_file = request.files['image']
+        image_file.save("temp_image.jpg")
+        result = image_to_text_pipe("temp_image.jpg")
+        os.remove("temp_image.jpg")
+        return jsonify({'result': result[0]['generated_text']})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
